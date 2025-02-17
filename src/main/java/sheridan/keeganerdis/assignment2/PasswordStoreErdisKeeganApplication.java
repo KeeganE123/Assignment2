1b5fe52c19ad12f6bb38348c6e3c30906647e4a3
KeeganE123 <156242145+KeeganE123@users.noreply.github.com>
2025-02-17 14:26:38 -0500

    Project Created

---
+++ src/main/java/sheridan/keeganerdis/assignment2/PasswordStoreErdisKeeganApplication.java
@@ -0,0 +1,13 @@
+package sheridan.keeganerdis.assignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordStoreErdisKeeganApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordStoreErdisKeeganApplication.class, args);
+    }
+
+}
